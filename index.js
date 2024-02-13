e98472c513e16c604f115b3ea221a0e6aa563b21
ralipio <rolfe.alipio@orro.group>
2024-02-13 16:01:58 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('readNum').onclick = convertToNumWords(number) {
+    const numLength = number.Length()
+    console.log(`okok`)
+}
+
+let num = document.getElementById('readNum')
+convertToNumWords(num)
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number to Number Words</title>
+    <link rel="stylesheet"; href="./style.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div  class= "container">
+        <input id="readNum" type="number" placeholder="Number"/>
+        <button id="get-number"> Convert </button>
+        <div class="worded-numbers">
+            
+        </div>
+    </div>
+</body>
+<script src="index.js">
+
+</script>
+</html>
